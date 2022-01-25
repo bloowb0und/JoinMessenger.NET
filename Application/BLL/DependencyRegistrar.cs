4d42706e30e7828e7e72af28c58251bc6a55630d
bloowb0und <bloowbound@gmail.com>
2022-01-25 02:04:25 +0200

    Added PL, BLL and BLL.Abstractions projects. Updated proj and lang versions.

---
+++ Application/BLL/DependencyRegistrar.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL
+{
+    public class DependencyRegistrar
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
