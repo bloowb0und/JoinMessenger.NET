af65a4e918e1884aa0ff6b9fbe8d5e69718e470d
bloowb0und <bloowbound@gmail.com>
2022-02-08 02:31:56 +0200

    Added migrations

---
+++ Application/DAL/Migrations/20220205205720_Initial.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    DefaultStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatPermissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServerPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    DefaultStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerPermissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(30)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    ServerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chats_Servers_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Servers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    ServerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Servers_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Servers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserServer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    ServerId = table.Column<int>(type: "int", nullable: true),
+                    DateEntered = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserServer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserServer_Servers_ServerId",
+                        column: x => x.ServerId,
+                        principalTable: "Servers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserServer_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    ChatId = table.Column<int>(type: "int", nullable: true),
+                    Value = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateLastEdited = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatPermissionRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    ChatPermissionId = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatPermissionRole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChatPermissionRole_ChatPermissions_ChatPermissionId",
+                        column: x => x.ChatPermissionId,
+                        principalTable: "ChatPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ChatPermissionRole_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChatPermissionRole_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServerPermissionRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    ServerPermissionId = table.Column<int>(type: "int", nullable: false),
+                    ServerPermissionStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerPermissionRole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServerPermissionRole_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ServerPermissionRole_ServerPermissions_ServerPermissionId",
+                        column: x => x.ServerPermissionId,
+                        principalTable: "ServerPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserServerRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserServerId = table.Column<int>(type: "int", nullable: true),
+                    RoleId = table.Column<int>(type: "int", nullable: true),
+                    DateApplied = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserServerRole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserServerRole_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserServerRole_UserServer_UserServerId",
+                        column: x => x.UserServerId,
+                        principalTable: "UserServer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatPermissionRole_ChatId",
+                table: "ChatPermissionRole",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatPermissionRole_ChatPermissionId",
+                table: "ChatPermissionRole",
+                column: "ChatPermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatPermissionRole_RoleId",
+                table: "ChatPermissionRole",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_ServerId",
+                table: "Chats",
+                column: "ServerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatId",
+                table: "Messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ServerId",
+                table: "Roles",
+                column: "ServerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServerPermissionRole_RoleId",
+                table: "ServerPermissionRole",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServerPermissionRole_ServerPermissionId",
+                table: "ServerPermissionRole",
+                column: "ServerPermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserServer_ServerId",
+                table: "UserServer",
+                column: "ServerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserServer_UserId",
+                table: "UserServer",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserServerRole_RoleId",
+                table: "UserServerRole",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserServerRole_UserServerId",
+                table: "UserServerRole",
+                column: "UserServerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatPermissionRole");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "ServerPermissionRole");
+
+            migrationBuilder.DropTable(
+                name: "UserServerRole");
+
+            migrationBuilder.DropTable(
+                name: "ChatPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+
+            migrationBuilder.DropTable(
+                name: "ServerPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "UserServer");
+
+            migrationBuilder.DropTable(
+                name: "Servers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Application/DAL/Migrations/20220207123916_FixedRequiredFields.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class FixedRequiredFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatPermissionRole_ChatPermissions_ChatPermissionId",
+                table: "ChatPermissionRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServerPermissionRole_ServerPermissions_ServerPermissionId",
+                table: "ServerPermissionRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserServerRole_Roles_RoleId",
+                table: "UserServerRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserServerRole_UserServer_UserServerId",
+                table: "UserServerRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ServerPermissions",
+                table: "ServerPermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ChatPermissions",
+                table: "ChatPermissions");
+
+            migrationBuilder.RenameTable(
+                name: "ServerPermissions",
+                newName: "ServerPermission");
+
+            migrationBuilder.RenameTable(
+                name: "ChatPermissions",
+                newName: "ChatPermission");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserServerId",
+                table: "UserServerRole",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "UserServerRole",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ServerPermission",
+                table: "ServerPermission",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ChatPermission",
+                table: "ChatPermission",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatPermissionRole_ChatPermission_ChatPermissionId",
+                table: "ChatPermissionRole",
+                column: "ChatPermissionId",
+                principalTable: "ChatPermission",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServerPermissionRole_ServerPermission_ServerPermissionId",
+                table: "ServerPermissionRole",
+                column: "ServerPermissionId",
+                principalTable: "ServerPermission",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserServerRole_Roles_RoleId",
+                table: "UserServerRole",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserServerRole_UserServer_UserServerId",
+                table: "UserServerRole",
+                column: "UserServerId",
+                principalTable: "UserServer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatPermissionRole_ChatPermission_ChatPermissionId",
+                table: "ChatPermissionRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServerPermissionRole_ServerPermission_ServerPermissionId",
+                table: "ServerPermissionRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserServerRole_Roles_RoleId",
+                table: "UserServerRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserServerRole_UserServer_UserServerId",
+                table: "UserServerRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ServerPermission",
+                table: "ServerPermission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ChatPermission",
+                table: "ChatPermission");
+
+            migrationBuilder.RenameTable(
+                name: "ServerPermission",
+                newName: "ServerPermissions");
+
+            migrationBuilder.RenameTable(
+                name: "ChatPermission",
+                newName: "ChatPermissions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserServerId",
+                table: "UserServerRole",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "UserServerRole",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ServerPermissions",
+                table: "ServerPermissions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ChatPermissions",
+                table: "ChatPermissions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatPermissionRole_ChatPermissions_ChatPermissionId",
+                table: "ChatPermissionRole",
+                column: "ChatPermissionId",
+                principalTable: "ChatPermissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServerPermissionRole_ServerPermissions_ServerPermissionId",
+                table: "ServerPermissionRole",
+                column: "ServerPermissionId",
+                principalTable: "ServerPermissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserServerRole_Roles_RoleId",
+                table: "UserServerRole",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserServerRole_UserServer_UserServerId",
+                table: "UserServerRole",
+                column: "UserServerId",
+                principalTable: "UserServer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
